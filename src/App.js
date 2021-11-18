b3a26ddce5612e3b168f46b437796161cd9965dd
congthai99 <thaicong328@gmail.com>
2021-11-18 14:58:54 +0700

    sua ten goi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world with Reactjs and Cong thai
         </p>
         <a
           className="App-link"
