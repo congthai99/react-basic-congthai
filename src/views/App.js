438091964b2b8a467bb9b432daa9eeecf3c50174
congthai99 <thaicong328@gmail.com>
2021-11-28 15:00:01 +0700

    Todo app

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.scss';
 import Mycomponent from './example/MyComponent';
+import ListTodo from './Todos/ListTodo';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 /**
  * 2 components : class component     / function component (function, arrow)
  * >>>>>>>>>>>>>: statefull component / stateless component
@@ -14,11 +17,26 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world with Reactjs and Cong thai
+          Simple Todo apps with Reactjs and Cong thai
         </p>
-        
-        <Mycomponent/>
+
+        {/* <Mycomponent/> */}
+        <ListTodo />
       </header>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      {/* Same as */}
+      <ToastContainer />
     </div>
   );
 }
